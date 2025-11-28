a3354269d4c73f5d13326e1d299d6f2f339d7b94
xDragon-Dev <dragonpunishment123@gmail.com>
2025-11-27 20:43:24 -0600

    Primera versión implementando la librería

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,51 @@
+use bevy::prelude::*;
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+#[derive(Resource)]
+struct GreetTimer(Timer);
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
+        .add_plugins(DefaultPlugins)
+        .add_plugins(HelloPlugin)
+        .run();
+}
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name(String::from("Juanito"))));
+    commands.spawn((Person, Name(String::from("Pedrito"))));
+    commands.spawn((Person, Name(String::from("Maria"))));
+    commands.spawn((Person, Name(String::from("José"))));
+}
+
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    if timer.0.tick(time.delta()).just_finished(){
+        for name in query {
+            println!("Hello {}!", name.0);
+        }
+    }
+}
+
+fn update_people(mut query: Query<&mut Name, With<Person>>) {
+    for mut name in &mut query {
+        if name.0 == "Juanito" {
+            name.0 = "Miguel".into();
+        }
+    }
+}
+
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app
+            .add_systems(Startup, add_people)
+            .add_systems(Update, (update_people, greet_people).chain());
+    }
 }
